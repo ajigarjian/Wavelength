6e7b9c6fe048fe6791c58c0238a6357008304ccf
Ari Jigarjian <ajigarjian@gmail.com>
2022-08-21 20:32:37 -0400

    Restructed project directory for easier development

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import random #import random library for spinner function
 
-def spin(): #package randint into a function called spin.
+#package randint into a function called spin
+def spin(): 
     return random.randint(1,100) #returns a random integer from 1 to 100, inclusive
-
-
-
