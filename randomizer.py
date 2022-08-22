f786fb3dc407e706860114610e06ad1e56d786f3
Ari Jigarjian <ajigarjian@gmail.com>
2022-08-21 20:06:15 -0400

    Added random int logic to python

---
+++ randomizer.py
@@ -0,0 +1,7 @@
+import random #import random library for spinner function
+
+def spin(): #package randint into a function called spin.
+    return random.randint(1,100) #returns a random integer from 1 to 100, inclusive
+
+
+
