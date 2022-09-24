8565b3a355015c93ed7ed03c7142b2b3f9a8e930
Ari Jigarjian <ajigarjian@gmail.com>
2022-09-23 23:35:17 -0400

    made dial responsive

--- static/app.js
+++ static/app.js
@@ -1,6 +1,3 @@
-
-
-//javascript function for dragging the dial. Guidance taken from here: https://www.petercollingridge.co.uk/tutorials/svg/interactive/dragging/
 function makeDraggable(evt) {
 
     //sets the elements in the svg as the target of the event
@@ -25,30 +22,11 @@ function makeDraggable(evt) {
         };
     }
 
-    // function getLineLength(evt) {
-
-    //     x1 = selectedElement.getAttributeNS(null, "x1");
-    //     x2 = selectedElement.getAttributeNS(null, "x2");
-
-    //     y1 = selectedElement.getAttributeNS(null, "y1");
-    //     y2 = selectedElement.getAttributeNS(null, "y2");
-
-    //     length = Math.sqrt(Math.pow((x1 - x2),2) + Math.pow((y1 - y2),2))
-
-    //     return {
-    //         length
-    //     };
-    // }
-
     // checks if the element has the CSS class 'draggable', if it does, it sets it to be the 'selectedElement'
     function startDrag(evt) {
         if (evt.target.classList.contains('draggable'))
         {
             selectedElement = evt.target;
-
-            offset = getMousePosition(evt);
-            offset.x -= parseFloat(selectedElement.getAttributeNS(null, "x2"));
-            offset.y -= parseFloat(selectedElement.getAttributeNS(null, "y2"));
         }
     }
 
@@ -62,24 +40,31 @@ function makeDraggable(evt) {
             // gets x and y coordinate of mouse
             var mousePos = getMousePosition(evt);
 
-            //adds offset to mouse position and stores into new variable
-            var newX = mousePos.x-offset.x;
+            //getting the values of the svg width, height, and circle radius as absolute values from percentages in html
+            var windowWidth = window.outerWidth;
+            var windowHeight = parseFloat((document.getElementById("dial_svg")).getAttributeNS(null, "height"));
+            var radiusPercentage = (parseFloat((document.getElementById("outer_circle")).getAttributeNS(null, "r")))/100.0
+            //for figuring out how percentages work as svg attributes: https://oreillymedia.github.io/Using_SVG/extras/ch05-percentages.html
+            var circleRadius = radiusPercentage*(Math.sqrt(Math.pow(windowWidth, 2) + Math.pow(windowHeight, 2))/Math.SQRT2)
+            
+            //stores location of mouse x coordinate in pixels
+            var newX = mousePos.x;
 
-            //if the mouse moves too far to the right, then cap it so the new y coordinate doesn't glitch
-            if (newX > 920) {
-                newX = 920;
+            //setting upper limit of x when moving dial
+            if (newX > ((windowWidth*0.5)+circleRadius)) {
+                newX = ((windowWidth*0.5)+circleRadius);
             }
 
-            //if the mouse moves too far to the left, then put a floor so the new y coordinate doesn't glitch
-            else if(newX < 520) {
-                newX = 520;
+            //setting lower limit of x when moving dial
+            else if (newX < ((windowWidth*0.5)-circleRadius)) {
+                newX = ((windowWidth*0.5)-circleRadius);
             }
-        
+
             //sets x2 attribute of selectedElement (in this case the line) to be the new X
             selectedElement.setAttributeNS(null, "x2", newX);
 
             //creates variable to store what the new y should be based on d = square root of ((x2-x1)^2 + (y2-y1)^2)
-            var newY = 250-Math.sqrt((40000 - Math.pow((newX - 720),2)))
+            var newY = windowHeight-Math.sqrt((Math.pow(circleRadius,2) - Math.pow((newX - windowWidth*0.5),2)))
 
             //sets y2 attribue of selectedElement (in this case the line) to be the new Y
             selectedElement.setAttributeNS(null, "y2", newY)
